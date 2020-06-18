76cf6f8270dc458d16549f0bf3f74acc9e882ec8
electrocatstudios <phil@electrocatstudios.com>
2020-06-18 19:31:33 +0100

    Combined the server and read only into memory

--- Camera/main.go
+++ Camera/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/dhowden/raspicam"
+	"github.com/gorilla/mux"
 )
 
 const OUTPUT_DIRECTORY = "output"
@@ -18,34 +25,33 @@ func setup() {
 	}
 }
 
-func main() {
-	fmt.Println("Starting camera service")
+type CameraImage struct {
+	Name   string
+	Lock   sync.Mutex
+	Buffer bytes.Buffer
+}
 
-	setup()
+type CameraImages struct {
+	CurrentImage *CameraImage
+	LoadingImage *CameraImage
+	Lock         sync.Mutex
+	Count        int
+}
+
+var CameraImagesBroker CameraImages
+
+func CaptureImages() {
 
 	for {
 		timestamp := time.Now().Unix()
+		photo_name := fmt.Sprintf("%d", timestamp)
 
-		filename := fmt.Sprintf("%s/%d.tmp", OUTPUT_DIRECTORY, timestamp)
-		final_fn := fmt.Sprintf("%s/%d.jpg", OUTPUT_DIRECTORY, timestamp)
-		lock_filename := fmt.Sprintf("%s/%d.lock", OUTPUT_DIRECTORY, timestamp)
-		lock_file, err := os.Create(lock_filename)
-		if err != nil {
-			fmt.Println("Failed to create lock file")
-			panic("Failed to create lock")
-			// return
+		if CameraImagesBroker.LoadingImage == nil {
+			CameraImagesBroker.LoadingImage = new(CameraImage)
 		}
-
-		lock_file.Close()
-
-		f, err := os.Create(filename)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "create file: %v", err)
-			panic("Failed to create file")
-			// return
-		}
-		// defer f.Close()
-
+		CameraImagesBroker.LoadingImage.Lock.Lock()
+		CameraImagesBroker.LoadingImage.Name = photo_name
+		w := bufio.NewWriter(&CameraImagesBroker.LoadingImage.Buffer)
 		s := raspicam.NewStill()
 		errCh := make(chan error)
 		go func() {
@@ -53,19 +59,80 @@ func main() {
 				fmt.Fprintf(os.Stderr, "%v\n", x)
 			}
 		}()
-		err = os.Remove(lock_filename)
-		if err != nil {
-			fmt.Println("Failed to remove lock")
-			panic("Failed to remove lock")
+
+		raspicam.Capture(s, w, errCh)
+
+		CameraImagesBroker.LoadingImage.Lock.Unlock()
+		bWasNil := true
+		var oldImage *CameraImage
+		if CameraImagesBroker.CurrentImage != nil {
+			bWasNil = false
+			oldImage = CameraImagesBroker.CurrentImage
+			oldImage.Lock.Lock()
 
 		}
-		// log.Println("Capturing image...")
-		raspicam.Capture(s, f, errCh)
-		f.Close()
 
-		os.Rename(filename, final_fn)
-		os.Chmod(final_fn, 0666)
+		CameraImagesBroker.Lock.Lock()
 
+		// Swap out the images while broker is locked
+		CameraImagesBroker.CurrentImage = CameraImagesBroker.LoadingImage
+		CameraImagesBroker.LoadingImage = nil
+		CameraImagesBroker.Count += 1
+		CameraImagesBroker.Lock.Unlock()
+
+		if !bWasNil {
+			oldImage.Lock.Unlock()
+		}
 	}
+}
+
+func getLatestImage(w http.ResponseWriter, r *http.Request) {
+	if CameraImagesBroker.CurrentImage == nil {
+		// TODO: Something better than this
+		return
+	}
+
+	CameraImagesBroker.CurrentImage.Lock.Lock()
+	// fmt.Println("Getting Image " + CameraImagesBroker.CurrentImage.Name)
+	w.Write(CameraImagesBroker.CurrentImage.Buffer.Bytes())
+	CameraImagesBroker.CurrentImage.Lock.Unlock()
+}
+
+type StatusResponse struct {
+	NumberImages int
+	Status       string
+}
+
+func getStatus(w http.ResponseWriter, r *http.Request) {
+	var resp StatusResponse
+
+	CameraImagesBroker.Lock.Lock()
+	resp.NumberImages = CameraImagesBroker.Count
+	CameraImagesBroker.Lock.Unlock()
+
+	resp.Status = "ok"
+	json.NewEncoder(w).Encode(&resp)
+}
+
+func getHomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
+}
+
+func main() {
+	fmt.Println("Starting camera service")
+
+	setup()
+
+	go CaptureImages()
+
+	fmt.Println("setting up router")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", getHomePage)
+	r.HandleFunc("/image", getLatestImage)
+	r.HandleFunc("/status", getStatus)
+	http.Handle("/", r)
+	fmt.Println("About to start server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
